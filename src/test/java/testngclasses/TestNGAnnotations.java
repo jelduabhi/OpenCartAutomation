ff9d67567f70dcd5ed545b1f553c3a9752badfda
abhitraining <abhitrainingcenter@gmail.com>
2024-08-23 11:07:13 +0530

    method name changed to test 5 in Sai branch

--- src/test/java/testngclasses/TestNGAnnotations.java
+++ src/test/java/testngclasses/TestNGAnnotations.java
@@ -25,7 +25,7 @@ public class TestNGAnnotations extends TestNGMethods {
 
 
     @Test(groups = {"regression","login"})
-    public void test4(){
+    public void test5(){
         System.out.println("TestNGAnnotations test 4");
         Assert.fail();
     }
